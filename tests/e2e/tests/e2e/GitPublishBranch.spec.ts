498c1f292c81484d9d0d3586c7c9220a71d8ef9f
Skoryk Serhii <sskoryk@redhat.com>
2022-08-30 18:16:46 +0300

    adapt GitSSh test

--- tests/e2e/tests/e2e/GitPublishBranch.spec.ts
+++ tests/e2e/tests/e2e/GitPublishBranch.spec.ts
@@ -49,7 +49,7 @@ import { ITestWorkspaceUtil } from '../../utils/workspace/ITestWorkspaceUtil';
   const fileName: string = 'README.md';
   const workspaceName: string = 'python-hello-world';
   const stackName: string = 'Python';
-  const sshKeyName: string = "publish-branch";
+  const sshKeyName: string = 'publish-branch';
 
   suiteTeardown(async () => {
       await testWorkspaceUtil.stopAndDeleteWorkspaceByName(workspaceName);
@@ -62,10 +62,10 @@ import { ITestWorkspaceUtil } from '../../utils/workspace/ITestWorkspaceUtil';
 
       test('Generate a SSH key', async () => {
           await topMenu.selectOption('View', 'Find Command...');
-         //  // workaround - reopen 'Find Command' container - https://github.com/eclipse/che/issues/19793
+          // workaround - reopen 'Find Command' container - https://github.com/eclipse/che/issues/19793
           await topMenu.selectOption('View', 'Find Command...');
           await quickOpenContainer.typeAndSelectSuggestion('SSH generate', 'SSH: Generate Key...');
-          await ide.waitNotificationAndClickOnButton('Key pair successfully generated, do you want to view the public key', 'View');
+          await ide.waitNotificationAndClickOnButton('Key pair successfully generated, do you want to view the public key', 'View', TimeoutConstants.TS_OPEN_EDITOR_TIMEOUT);
           await editor.waitEditorOpened('Untitled-0');
           await editor.waitText('Untitled-0', 'ssh-rsa');
       });
@@ -73,12 +73,10 @@ import { ITestWorkspaceUtil } from '../../utils/workspace/ITestWorkspaceUtil';
       test('Add a SSH key to GitHub side and clone by ssh link', async () => {
           await gitHubUtils.deletePublicSshKeyByName(TestConstants.TS_GITHUB_TEST_REPO_ACCESS_TOKEN, sshKeyName);
 
-          await driverHelper.getDriver().sleep(10000);
-
-        //   const publicSshKey = await editor.getEditorHiddenText('Untitled-0');
+            // const publicSshKey = await editor.getEditorHiddenText('Untitled-0');
           const publicSshKey = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDHf329lnI/hcoZIuiecBqhjN8k8DcFEXaYcqGv+tZpWgbH6Tq/sjEZvmNHRN9MBhu7q/iYBbzhU5o/18WTk8jLqz7eiyY+Pw8Kk4uxfFzmfzeLo6t32gIf5nKJrRab0H9/SiW6pmGo/jZTSgPQRypgtsY04Ztk3q7I8t0C0/Jj8YrdkRZb80zb69vsmNkFYkAKshmVUOC+eKX7oQL2uZ6C+UwCKz1TokDlfG21/eEG5qwfzQJcLTBFF9oH17/ZjLurC5vZoC7iT6fHXTQAae411qVKGSAsuEu9zJFynODUZEeblVdtat4t/CmfOAcjndFc17BuGS7ZDe/GBE5PxmPFp/XgeZn/QC8WxrKmTaldApCUN5/9Gx5IVuR1XJzCpbZ2YzJKXQ/r17DFjH8UrBTndb7mowKnVI1BR9sF9J0xZr9oG35t6QkEvFEXx1gXFHIUz6JwiR1yrqaXvAeemHMZtLIrxpVsClkFTGhPrrVPcUGTn5LvRafmRkE9Abl6U83A5ZZpOjBPYdcO3qjvUnkJPrG9g4mXrYCeoH+0WzE//FXnxqBAh0mDro2L2d9ehDTxIgw9N0f2yn7wqXuAWWOVXYrQTvEF8L+1CDAI5NuIxkoOwGDw1BDMnJLBjedCju5n1B8MZ8Y0OZecmey1oThN/Kke2GiL+/H3ULV4/aVBRQ== root@workspacedbde154b5be945b9-7c9cc566cf-pkrsv';
 
-          console.log(publicSshKey);
+          console.log('>>' + publicSshKey + '<<');
           await gitHubUtils.addPublicSshKeyToUserAccount(TestConstants.TS_GITHUB_TEST_REPO_ACCESS_TOKEN, sshKeyName, publicSshKey);
           await cloneTestRepo();
           await driverHelper.wait(TimeoutConstants.TS_IMPORT_PROJECT_DEFAULT_POLLING);
--- tests/e2e/tests/e2e/GitSsh.spec.ts
+++ tests/e2e/tests/e2e/GitSsh.spec.ts
@@ -1,5 +1,5 @@
 /*********************************************************************
- * Copyright (c) 2019 Red Hat, Inc.
+ * Copyright (c) 2022 Red Hat, Inc.
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -16,81 +16,66 @@ import { GitPlugin } from '../../pageobjects/ide/plugins/GitPlugin';
 import { Ide } from '../../pageobjects/ide/Ide';
 import { ProjectTree } from '../../pageobjects/ide/ProjectTree';
 import { QuickOpenContainer } from '../../pageobjects/ide/QuickOpenContainer';
-import { ICheLoginPage } from '../../pageobjects/login/ICheLoginPage';
 import { TestConstants } from '../../TestConstants';
 import { DriverHelper } from '../../utils/DriverHelper';
-import { WorkspaceNameHandler } from '../../utils/WorkspaceNameHandler';
-import { CheGitApi } from '../../utils/VCS/CheGitApi';
 import { GitHubUtil } from '../../utils/VCS/github/GitHubUtil';
 import { TopMenu } from '../../pageobjects/ide/TopMenu';
 import { TimeoutConstants } from '../../TimeoutConstants';
-import { BrowserTabsUtil } from '../../utils/BrowserTabsUtil';
-import { Dashboard } from '../..';
 import { ITestWorkspaceUtil } from '../../utils/workspace/ITestWorkspaceUtil';
 import { WorkspaceHandlingTests } from '../../testsLibrary/WorkspaceHandlingTests';
-import CheReporter from '../../driver/CheReporter';
+import { ProjectAndFileTests } from '../../testsLibrary/ProjectAndFileTests';
+import { Dashboard } from '../../pageobjects/dashboard/Dashboard';
 
 const driverHelper: DriverHelper = e2eContainer.get(CLASSES.DriverHelper);
 const ide: Ide = e2eContainer.get(CLASSES.Ide);
 const quickOpenContainer: QuickOpenContainer = e2eContainer.get(CLASSES.QuickOpenContainer);
 const editor: Editor = e2eContainer.get(CLASSES.Editor);
 const topMenu: TopMenu = e2eContainer.get(CLASSES.TopMenu);
-const loginPage: ICheLoginPage = e2eContainer.get<ICheLoginPage>(TYPES.CheLogin);
 const gitHubUtils: GitHubUtil = e2eContainer.get<GitHubUtil>(CLASSES.GitHubUtil);
-const cheGitAPI: CheGitApi = e2eContainer.get(CLASSES.CheGitApi);
 const projectTree: ProjectTree = e2eContainer.get(CLASSES.ProjectTree);
 const gitPlugin: GitPlugin = e2eContainer.get(CLASSES.GitPlugin);
-const testWorkspaceUtils: ITestWorkspaceUtil = e2eContainer.get<ITestWorkspaceUtil>(TYPES.WorkspaceUtil);
+const workspaceHandlingTests: WorkspaceHandlingTests = e2eContainer.get(CLASSES.WorkspaceHandlingTests);
+const projectAndFileTests: ProjectAndFileTests = e2eContainer.get(CLASSES.ProjectAndFileTests);
+const testWorkspaceUtil: ITestWorkspaceUtil = e2eContainer.get<ITestWorkspaceUtil>(TYPES.WorkspaceUtil);
 const dashboard: Dashboard = e2eContainer.get(CLASSES.Dashboard);
-const browserTabsUtil: BrowserTabsUtil = e2eContainer.get(CLASSES.BrowserTabsUtil);
-const workspaceNameHandler: WorkspaceNameHandler = e2eContainer.get(CLASSES.WorkspaceNameHandler);
-
-const workspacePrefixUrl: string = `${TestConstants.TS_SELENIUM_BASE_URL}/dashboard/#/ide/${TestConstants.TS_SELENIUM_USERNAME}/`;
-const wsNameCheckGeneratingKeys = 'checkGeneratingSsh';
-const wsNameCheckPropagatingKeys = 'checkPropagatingSsh';
 const committedFile = 'README.md';
 
-suite('Git with ssh workflow', async () => {
-    suiteSetup(async function () {
-        const wsConfig = await testWorkspaceUtils.getBaseDevfile();
-        wsConfig.metadata!.name = wsNameCheckGeneratingKeys;
-        await browserTabsUtil.navigateTo(TestConstants.TS_SELENIUM_BASE_URL);
-        await loginPage.login();
-        await testWorkspaceUtils.createWsFromDevFile(wsConfig);
-    });
+const projectName: string = 'TestRepo';
+const fileName: string = 'README.md';
+const workspaceName: string = 'python-hello-world';
+const stackName: string = 'Python';
+const sshKeyName: string = 'git-ssh';
 
-    test('Login into workspace and open tree container', async () => {
-        await dashboard.openDashboard();
-        await browserTabsUtil.navigateTo(workspacePrefixUrl + wsNameCheckGeneratingKeys);
-        WorkspaceHandlingTests.setWorkspaceName(wsNameCheckGeneratingKeys);
-        CheReporter.registerRunningWorkspace(wsNameCheckGeneratingKeys);
-        await ide.waitWorkspaceAndIde();
-        await projectTree.openProjectTreeContainer();
-        await driverHelper.wait(TimeoutConstants.TS_PROJECT_TREE_TIMEOUT);
-    });
+suite('Git with ssh workflow', async () => {
+    workspaceHandlingTests.createAndOpenWorkspace(stackName);
+
+    projectAndFileTests.waitWorkspaceReadiness(workspaceName, fileName);
 
     test('Generate a SSH key', async () => {
         await topMenu.selectOption('View', 'Find Command...');
         // workaround - reopen 'Find Command' container - https://github.com/eclipse/che/issues/19793
         await topMenu.selectOption('View', 'Find Command...');
-        await quickOpenContainer.typeAndSelectSuggestion('SSH', 'SSH: Generate Key...');
-        await ide.waitNotificationAndClickOnButton('Key pair successfully generated, do you want to view the public key', 'View');
+        await quickOpenContainer.typeAndSelectSuggestion('SSH generate', 'SSH: Generate Key...');
+        await ide.waitNotificationAndClickOnButton('Key pair successfully generated, do you want to view the public key', 'View', TimeoutConstants.TS_OPEN_EDITOR_TIMEOUT);
         await editor.waitEditorOpened('Untitled-0');
         await editor.waitText('Untitled-0', 'ssh-rsa');
     });
 
-
     test('Add a SSH key to GitHub side and clone by ssh link', async () => {
-        const sshName: string = workspaceNameHandler.generateWorkspaceName('test-SSH-', 5);
-        const publicSshKey = await cheGitAPI.getPublicSSHKey();
-        await gitHubUtils.addPublicSshKeyToUserAccount(TestConstants.TS_GITHUB_TEST_REPO_ACCESS_TOKEN, sshName, publicSshKey);
-        await cloneTestRepo();
+        await gitHubUtils.deletePublicSshKeyByName(TestConstants.TS_GITHUB_TEST_REPO_ACCESS_TOKEN, sshKeyName);
 
+          // const publicSshKey = await editor.getEditorHiddenText('Untitled-0');
+        const publicSshKey = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC3F5RRp92qHSBk72wGGFLRKBQ8N+WhRvIeca1c8HkJJRMzZvc9jBDaz2gMwSLfg/rmeMseJVT7QvI6vmBYA0UqlOgjt/S3nOQCxl5WbLGDsOhDHjeEuhaSOM+KaqHewLf7srGQEfzZlXhMrhxB0sM+JU/gMbCf2aNg5zh/xm5Zeu87w3XX6wMXAZx7Iwah0tDLednnsNEmCppKQGRNZLu7awDV5fX3TIYD+G4xpq+WduGaqOoMOmG/OANomDkAkWKkcPPz8mppfQr7lCeJK1uPYMLxSROkPrvfYzTXN4MzeC55HEHVVane6JB5iBTPJDhMrSTyfkhfL65xtj1hRVZYSnibdlLKJ7/WuF5gjP3C6hEAKIiQP/88ewLQc8jgMl/3XxUgDi1E7z7M18n1TjyH8jPpvrE97AEHSRBPi9GhZUIG7bBJhhRZLQl6gYvOVqXtZ9223AvYRBcayE4GpSFn0V/hkMloOG+Ik97vftr5GpDrtAy/hX9lMulds9j7gHJZy9DmbONzM/VD92XAB5+2bzH5qotGWZwmXoCTt2URsOcDKglzuUWnTJksKx8H/gGqGgyHP3TxFkkYNptJp/Da6krxy0Kj3ogyBgpGiqESRnRKqjMCOn9+cL5eBiu3UV6JgZ0Jo1QT82cTAMlZ0skUxOToR/TD6Eyp6d/pMX7tIw== root@workspace413ca643206e4b6d-5649c4f894-f9prj';
+
+        console.log('>>' + publicSshKey + '<<');
+        await gitHubUtils.addPublicSshKeyToUserAccount(TestConstants.TS_GITHUB_TEST_REPO_ACCESS_TOKEN, sshKeyName, publicSshKey);
+        await cloneTestRepo();
+        await driverHelper.wait(TimeoutConstants.TS_IMPORT_PROJECT_DEFAULT_POLLING);
     });
 
     test('Change commit and push', async function changeCommitAndPushFunc() {
         const currentDate: string = Date.now().toString();
-        await projectTree.expandPathAndOpenFile('Spoon-Knife', committedFile);
+        await projectTree.expandPathAndOpenFile(projectName, committedFile);
         await editor.type(committedFile, currentDate + '\n', 1);
         await gitPlugin.openGitPluginContainer();
         await gitPlugin.waitChangedFileInChagesList(committedFile);
@@ -103,42 +88,29 @@ suite('Git with ssh workflow', async () => {
         await driverHelper.wait(TimeoutConstants.TS_EDITOR_TAB_INTERACTION_TIMEOUT);
         const rawDataFromFile: string = await gitHubUtils.getRawContentFromFile(TestConstants.TS_GITHUB_TEST_REPO + '/master/' + committedFile);
         assert.isTrue(rawDataFromFile.includes(currentDate));
-        await testWorkspaceUtils.cleanUpAllWorkspaces();
+
+        await testWorkspaceUtil.stopAndDeleteWorkspaceByName(workspaceName);
+        await dashboard.openDashboard();
     });
+
+    workspaceHandlingTests.createAndOpenWorkspace(stackName);
+    projectAndFileTests.waitWorkspaceReadiness(workspaceName, fileName);
 
     test('Check ssh key in  a new workspace', async () => {
-        const data = await testWorkspaceUtils.getBaseDevfile();
-
-        data.metadata!.name = wsNameCheckPropagatingKeys;
-        await testWorkspaceUtils.createWsFromDevFile(data);
-        await dashboard.openDashboard();
-        await browserTabsUtil.navigateTo(workspacePrefixUrl + wsNameCheckPropagatingKeys);
-        WorkspaceHandlingTests.setWorkspaceName(wsNameCheckPropagatingKeys);
-        CheReporter.registerRunningWorkspace(wsNameCheckGeneratingKeys);
-        await ide.waitWorkspaceAndIde();
-        await projectTree.openProjectTreeContainer();
-        await driverHelper.wait(TimeoutConstants.TS_PROJECT_TREE_TIMEOUT);
         await cloneTestRepo();
-        await projectTree.expandPath('Spoon-Knife');
-        await projectTree.waitItem('Spoon-Knife/README.md');
-    });
+        await projectTree.expandPathAndOpenFile(projectName, committedFile);
 
-});
-
-suite('Cleanup', async () => {
-    test('Remove test workspace', async () => {
-        await testWorkspaceUtils.cleanUpAllWorkspaces();
+        await testWorkspaceUtil.stopAndDeleteWorkspaceByName(workspaceName);
     });
 });
 
 async function cloneTestRepo() {
     const sshLinkToRepo: string = 'git@github.com:' + TestConstants.TS_GITHUB_TEST_REPO + '.git';
     const confirmMessage = 'Clone from URL, ' + sshLinkToRepo;
-
-    await topMenu.selectOption('View', 'Find Command...');
-    // workaround - reopen 'Find Command' container - https://github.com/eclipse/che/issues/19793
     await topMenu.selectOption('View', 'Find Command...');
     await quickOpenContainer.typeAndSelectSuggestion('clone', 'Git: Clone');
     await quickOpenContainer.typeAndSelectSuggestion(sshLinkToRepo, confirmMessage);
     await gitPlugin.clickOnSelectRepositoryButton();
+
+    await ide.waitNotificationAndClickOnButton('Would you like to open the cloned repository, or add it to the current workspace?', 'Open');
 }
